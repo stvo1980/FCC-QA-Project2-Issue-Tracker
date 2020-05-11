7e195bf145c97e20429fef3c9fa1e53aafb0fab7
Glitch (ribbon-fluorescent-hide) <none>
2020-05-11 21:02:58 +0000

    👾🌯 Checkpoint ./routes/api.js:15407664/936

--- routes/api.js
+++ routes/api.js
@@ -20,7 +20,35 @@ module.exports = function (app) {
   
     .get(function (req, res){
       var project = req.params.project;
-      
+      var issue = {
+        issue_title: req.body.issue_title,
+        issue_text: req.body.issue_text,
+        created_on: new Date(),
+        updated_on: new Date(),
+        created_by: req.body.created_by,
+        assigned_to: req.body.assigned_to || '',
+        open: true,
+        status_text: req.body.status_text || ''
+      };
+       if(!issue.issue_title || !issue.issue_text || !issue.created_by) {
+        res.send('missing inputs');
+      } else {
+        MongoClient.connect(CONNECTION_STRING, function(err, db) {
+      //   var db = db.db("test");
+          var collection = db.collection(project);
+          collection.insertOne(issue,function(err,doc){
+            if(err){console.log("Database error: " + err);}
+            else {
+    console.log("Successful database connection");}
+            
+            
+            issue._id = doc.insertedId;
+            res.json(issue);
+          });
+        });
+      }
+    
+    
     })
     
     .post(function (req, res){
