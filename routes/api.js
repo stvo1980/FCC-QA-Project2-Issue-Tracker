2caa85767c961adf248e6dfab198af2d7325d5d4
Glitch (ribbon-fluorescent-hide) <none>
2020-05-12 02:22:35 +0000

    Rewound to commit f9370ee6deca314f46ba92eeb400ad9dab883c52 + 0 edits

--- routes/api.js
+++ routes/api.js
@@ -75,10 +75,12 @@ console.log("DB updated")
     .put(function (req, res){
       var project = req.params.project;
       var issue = req.body._id;
+     delete req.body._id;
   // 
     var updates = req.body;
-    console.log(updates);
+    
      for (var ele in updates) { if (!updates[ele]) { delete updates[ele] } }
+    console.log("updates",updates);
 //     console.log(updates);
     updates.updated_on = new Date();
   MongoClient.connect(CONNECTION_STRING, function(err, db) {
@@ -87,18 +89,10 @@ console.log("DB updated")
     var db = db.db("test");
           var collection = db.collection(project);
     
-    collection.findAndModify({_id:new ObjectId(issue)}, 
-                             {$set: updates},
-                             {issue_text:req.body.issue_text,
-                              issue_title: req.body.issue_title,
-                            updated_on: new Date(),
-        created_by: req.body.created_by,
-        assigned_to: req.body.assigned_to,
-        status_text: req.body.status_text
-                             },
-                             {new: true},function(err,doc){
-      (!err) ? res.send('successfully updated') : res.send('could not update '+issue+' '+err);
-    })
+ collection.findAndModify({_id:new ObjectId(issue)},[['_id',1]],{$set: updates},{new: true},function(err,doc){
+            (!err) ? res.send('successfully updated') : res.send('could not update '+issue+' '+err);
+            //console.log(doc.value);
+          });
  
   
   })
