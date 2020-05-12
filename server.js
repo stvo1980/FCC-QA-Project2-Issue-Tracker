b6766b6c7377658417fd33d69ad949bdbc61442f
Glitch (ribbon-fluorescent-hide) <none>
2020-05-12 15:53:01 +0000

    📷⛑ Checkpoint ./server.js:15407664/90

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ var apiRoutes         = require('./routes/api.js');
 var fccTestingRoutes  = require('./routes/fcctesting.js');
 var runner            = require('./test-runner');
 
+
+const methodOverride = require('method-override');
+app.use(methodOverride('_method'));
+
+
 var app = express();
 
 app.use('/public', express.static(process.cwd() + '/public'));
