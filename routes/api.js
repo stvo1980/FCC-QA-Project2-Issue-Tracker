b08f9b7b9788aa1f2f4e6e431a22dd1a131b833a
Glitch (ribbon-fluorescent-hide) <none>
2020-05-12 13:51:54 +0000

    🏖💖 Checkpoint ./routes/api.js:15407664/8260

--- routes/api.js
+++ routes/api.js
@@ -1,132 +1,127 @@
 /*
-*
-*
-*       Complete the API routing below
-*
-*
-*/
+ *
+ *
+ *       Complete the API routing below
+ *
+ *
+ */
 
-'use strict';
+"use strict";
 
-var expect = require('chai').expect;
-var MongoClient = require('mongodb');
-var ObjectId = require('mongodb').ObjectID;
-var shortId = require("shortid")
+var expect = require("chai").expect;
+var MongoClient = require("mongodb");
+var ObjectId = require("mongodb").ObjectID;
+var shortId = require("shortid");
 
 const CONNECTION_STRING = process.env.DB; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
 
-module.exports = function (app,db) {
+module.exports = function(app, db) {
+  app
+    .route("/api/issues/:project")
 
-  app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+    .get(function(req, res) {
       var project = req.params.project;
-     
-    
-    
     })
-    
-    .post(function (req, res){
+
+    .post(function(req, res) {
       var project = req.params.project;
-    console.log(project)
-       var issue = {
+      console.log(project);
+      var issue = {
         issue_title: req.body.issue_title,
         issue_text: req.body.issue_text,
         created_on: new Date(),
         updated_on: new Date(),
         created_by: req.body.created_by,
-        assigned_to: req.body.assigned_to || '',
-     //    _id: shortId.generate,
+        assigned_to: req.body.assigned_to || "",
+        //    _id: shortId.generate,
         open: true,
-        status_text: req.body.status_text || ''
+        status_text: req.body.status_text || ""
       };
-       if(!issue.issue_title || !issue.issue_text || !issue.created_by) {
-        res.send('missing inputs');
-      } else {
-        MongoClient.connect(CONNECTION_STRING, function(err, db) {
-          
-          
-         var db = db.db("test");
-          var collection = db.collection(project);
-          collection.insertOne(issue,function(err,doc){
-      //      issue._id = doc.insertedId;
-        res.json({
-   //     _id: issue._id,
-        issue_title: issue.issue_title,
-        issue_text: issue.issue_text,
-        created_on: issue.created_on,
-        updated_on: issue.updated_on,
-        created_by: issue.created_by,
-        assigned_to: issue.assigned_to,
-        open: issue.open,
-        status_text: issue.status_text 
-            });
-console.log("DB updated")
-          });
-        });
-      }
-    
-    
-    
-    
-    
-    })
-    
-    .put(function (req, res){
-      var project = req.params.project;
-      var issue = req.body._id;
-     delete req.body._id;
-  
-    var updates = req.body;
-    
-     for (var i in updates) { if (!updates[i]) { delete updates[i] } }
-    console.log("updates",updates);
-//     console.log(updates);
-    updates.updated_on = new Date();
- 
-   console.log("length", Object.keys(updates).length); 
-    
-    if (Object.keys(updates).length < 2) 
-    {
-        res.send('no updated field sent');
-      } else {
-  MongoClient.connect(CONNECTION_STRING, function(err, db) {
-    
-    
-    var db = db.db("test");
-          var collection = db.collection(project);
-    
- collection.findOneAndUpdate({_id:new ObjectId(issue)},
-                     //     [['_id',1]],
-                          {$set: updates},
-                      //    {new: true},
-                          function(err,doc){
-            (!err) ? res.send('successfully updated') : res.send('could not update '+issue+' '+err);
-   
-          });
- 
-  
-  })
-  }
-    })
-    
-    .delete(function (req, res){
-      var project = req.params.project;
-       var issue = req.body._id;
-      if (!issue) {
-        res.send('_id error');
+      if (!issue.issue_title || !issue.issue_text || !issue.created_by) {
+        res.send("missing inputs");
       } else {
         MongoClient.connect(CONNECTION_STRING, function(err, db) {
           var db = db.db("test");
           var collection = db.collection(project);
-          collection.findAndRemove({_id:new ObjectId(issue)},function(err,doc){
-            (!err) ? res.send('deleted '+issue) : res.send('could not delete '+issue+' '+err);
+          collection.insertOne(issue, function(err, doc) {
+            //      issue._id = doc.insertedId;
+            res.json({
+              //     _id: issue._id,
+              issue_title: issue.issue_title,
+              issue_text: issue.issue_text,
+              created_on: issue.created_on,
+              updated_on: issue.updated_on,
+              created_by: issue.created_by,
+              assigned_to: issue.assigned_to,
+              open: issue.open,
+              status_text: issue.status_text
+            });
+            console.log("DB updated");
           });
         });
       }
-    
-    
-    
+    })
+
+    .put(function(req, res) {
+      var project = req.params.project;
+      var issue = req.body._id;
+      delete req.body._id;
+
+      var updates = req.body;
+
+      for (var i in updates) {
+        if (!updates[i]) {
+          delete updates[i];
+        }
+      }
+      console.log("updates", updates);
+      //     console.log(updates);
+      updates.updated_on = new Date();
+
+      console.log("length", Object.keys(updates).length);
+
+      if (Object.keys(updates).length < 2) {
+        res.send("no updated field sent");
+      } else {
+        MongoClient.connect(CONNECTION_STRING, function(err, db) {
+          var db = db.db("test");
+          var collection = db.collection(project);
+
+          collection.findOneAndUpdate(
+            { _id: new ObjectId(issue) },
+            //     [['_id',1]],
+            { $set: updates },
+            //    {new: true},
+            function(err, doc) {
+              !err
+                ? res.send("successfully updated")
+                : res.send("could not update " + issue + " " + err);
+            }
+          );
+        });
+      }
+    })
+
+    .delete(function(req, res) {
+      var project = req.params.project;
+    console.log("project",project);
+      var issue = req.body._id;
+      MongoClient.connect(CONNECTION_STRING, function(err, db) {
+       if(err){console.log("error connection delete",err)}
+        var db = db.db("test");
+        var collection = db.collection("apitest");
+
+        collection.findAndRemove(
+          { _id: new ObjectId(issue) },
+          //     [['_id',1]],
+          //    {$set: updates},
+          //    {new: true},
+          function(err, doc) {
+            !err
+              ? res.send("successfully deleted")
+              : res.send("could not delete " + issue + " " + err);
+          }
+        );
+      });
     });
-    
 };
